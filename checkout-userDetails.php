549d2936b6c6981e784aa4c45f510b3f9ce86215
Varsha Ubhrani <ubhrani.varsha@gmail.com>
2017-04-22 15:46:38 -0700

    final changes

--- checkout-userDetails.php
+++ checkout-userDetails.php
@@ -2,7 +2,6 @@
 require 'common.php';
 require 'config.php';
  $token  = $_POST['stripeToken'];
- $total=$_GET['total']*100;
 
   $customer = \Stripe\Customer::create(array(
       'email' => 'ubhrani.varsha@gmail.com',
@@ -11,7 +10,7 @@ require 'config.php';
 
   $charge = \Stripe\Charge::create(array(
       'customer' => $customer->id,
-      'amount'   => $total,
+      'amount'   => 5000,
       'currency' => 'usd'
   ));
 
