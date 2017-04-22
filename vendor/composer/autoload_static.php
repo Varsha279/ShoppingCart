b2cda9162c97c00d731ccea6000d49e4062c1bb5
Varsha Ubhrani <ubhrani.varsha@gmail.com>
2017-04-22 13:36:44 -0700

    email

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,25 +6,25 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit7942d451a69be9d766b4e462690bcfa8
 {
-    public static $prefixLengthsPsr4 = array (
-        'S' => 
-        array (
-            'Stripe\\' => 7,
-        ),
+    public static $files = array (
+        '3f8bdd3b35094c73a26f0106e3c0f8b2' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/SendGrid.php',
+        '9dda55337a76a24e949fbcc5d905a2c7' => __DIR__ . '/..' . '/sendgrid/sendgrid/lib/helpers/mail/Mail.php',
     );
 
-    public static $prefixDirsPsr4 = array (
-        'Stripe\\' => 
+    public static $prefixesPsr0 = array (
+        'S' => 
         array (
-            0 => __DIR__ . '/..' . '/stripe/stripe-php/lib',
+            'SendGrid' => 
+            array (
+                0 => __DIR__ . '/..' . '/sendgrid/php-http-client/lib',
+            ),
         ),
     );
 
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
-            $loader->prefixLengthsPsr4 = ComposerStaticInit7942d451a69be9d766b4e462690bcfa8::$prefixLengthsPsr4;
-            $loader->prefixDirsPsr4 = ComposerStaticInit7942d451a69be9d766b4e462690bcfa8::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit7942d451a69be9d766b4e462690bcfa8::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
